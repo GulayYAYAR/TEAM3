870b68c2c129efa37a5515c7c73522336c9245bc
seraytugcu <seraytugcu@outlook.com>
2022-07-28 22:40:10 -0400

    in line 2 space removed

--- src/Test_Seray/test2.java
+++ src/Test_Seray/test2.java
@@ -1,6 +1,7 @@
 package Test_Seray;
-public class Test1 {
+
+public class test2 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("seray");
     }
 }
