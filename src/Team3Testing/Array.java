b8726d3d988c986ebfcb9f066ef6a96df2d0afde
gulay <gulayyayar@gmail.com>
2022-07-28 23:29:18 -0400

    new

--- src/Team3Testing/Array.java
+++ src/Team3Testing/Array.java
@@ -2,11 +2,11 @@ package Team3Testing;
 
 public class Array {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Array:");
         for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + "  ");
+            System.out.print(arr[i] + "  ");;
 
         System.out.println();
 
@@ -14,7 +14,7 @@ public class Array {
         System.out.println("Reversed of Array: ");
 
         for (int i = arr.length - 1; i >= 0; i--)
-            System.out.print(arr[i] + "  ");
+            System.out.print(arr[i] + "  ");;
     }
 }
 
